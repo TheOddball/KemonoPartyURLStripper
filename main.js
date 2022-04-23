3d6cab4af37f288a189f1520604d7a2cd90861e4
The Oddball <oddball@oddball.tf>
2022-04-22 17:32:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const { exec } = require("child_process");
 const { argv } = require('process');
 const file = require('fs');
-let path = require('path');
-file.writeFileSync('./callback1.txt', "false");
-file.writeFileSync('./callback2.txt', "false");
-file.writeFileSync('./callback3.txt', "false");
-file.writeFileSync('./user.txt', "");
+const path = require('path');
+const callbackfiles = ["./callback1.txt", "./callback2.txt", "./callback3.txt", "./user.txt", "./links.json", "./pages.json", "./posts.json"];
+const args = argv.slice(2);
 
-var args = argv.slice(2);
+//Write temp files
+callbackfiles.forEach(
+    function (name) {
+        file.writeFileSync(name, "false")
+    }
+);
 
 for (let i = 0; i < args.length; i++) {
     console.log("Running script for " + args[i]);
@@ -15,51 +18,46 @@ for (let i = 0; i < args.length; i++) {
 
     // Get pages
     file.writeFileSync('./user.txt', args[i])
-    exec('slimerjs ./modules/getpages.js', (err, stdout, stderr) => {
+    exec('slimerjs ./modules/getpages.js', (err) => {
         if (err) {
-            console.error(err);
             return;
         }
-        console.log(stdout);
-        console.log(stderr);
     });
     file.writeFileSync('./callback1.txt', "false")
-    do {} while (file.readFileSync('./callback1.txt', 'utf8') !== "true");
+    do { } while (file.readFileSync('./callback1.txt', 'utf8') !== "true");
     file.writeFileSync('./user.txt', "")
 
     // Get posts
-    exec('slimerjs ./modules/getposts.js', (err, stdout, stderr) => {
+    exec('slimerjs ./modules/getposts.js', (err) => {
         if (err) {
-            console.error(err);
             return;
         }
-        console.log(stdout);
-        console.log(stderr);
     });
-    do {} while (file.readFileSync('./callback2.txt', 'utf8') !== "true");
+    do { } while (file.readFileSync('./callback2.txt', 'utf8') !== "true");
     file.writeFileSync('./callback2.txt', "false")
 
     // Get links
-    exec('slimerjs ./modules/getdrivelinks.js', (err, stdout, stderr) => {
+    exec('slimerjs ./modules/getdrivelinks.js', (err) => {
         if (err) {
-            console.error(err);
             return;
         }
-        console.log(stdout);
-        console.log(stderr);
     });
-    do {} while (file.readFileSync('./callback3.txt', 'utf8') !== "true");
+    do { } while (file.readFileSync('./callback3.txt', 'utf8') !== "true");
     file.writeFileSync('./callback3.txt', "false")
-    file.writeFileSync('./links_' + i + '.json', file.readFileSync('./links.json', 'utf8'));
-    console.log("Done! File saved as ./links_" + i + ".json");
+    if (file.readFileSync('./links.json', 'utf8') !== `[""]`) {
+        file.writeFileSync('./links_' + i + '.json', file.readFileSync('./links.json', 'utf8'));
+        console.log("Done! File saved as ./links_" + i + ".json for " + args[i] + "!");
+    } else {
+        console.log("No links found for " + args[i] + "!");
+    }
 }
+
+//Delete temp files
 function deleteFiles(name) {
     file.unlinkSync(path.join(__dirname, name));
 }
-deleteFiles('./callback1.txt');
-deleteFiles('./callback2.txt');
-deleteFiles('./callback3.txt');
-deleteFiles('./user.txt');
-deleteFiles('./links.json');
-deleteFiles('./pages.json');
-deleteFiles('./posts.json');
+callbackfiles.forEach(
+    function (name) {
+        deleteFiles(name);
+    }
+);
